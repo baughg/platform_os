25a808d9dd41a527d73885ab25d3bcbaf9bd5813
Gary Baugh <baughg@tcd.ie>
2020-05-17 19:52:19 +0100

    Implement directory_content

--- platform_os/WindowsPlatform.cpp
+++ platform_os/WindowsPlatform.cpp
@@ -35,8 +35,7 @@ bool WindowsPlatform::run_application(
 	STARTUPINFO startupInfo{};
 	startupInfo.cb = sizeof(startupInfo);
 	startupInfo.wShowWindow = SW_HIDE;
-	//startupInfo.wShowWindow = SW_SHOWNORMAL;
-
+	
 	BOOL result{};
 	//Needed since CreateProcessW may change the contents of CmdLine
 	std::array<TCHAR, MAX_PATH << 1 > tempCmdLine{};
@@ -71,6 +70,45 @@ uint32_t WindowsPlatform::directory_content(
 	const std::string &dir,
 	std::vector<std::string> &file_list,
 	std::vector<std::string> &dir_list) {
+	std::array<char, _MAX_PATH> original_directory{};
+
+	// Get the current directory so we can return to it
+	_getcwd(original_directory.data(), _MAX_PATH);
+
+	_chdir(dir.c_str());  // Change to the working directory
+	_finddata_t fileinfo;
+
+	// This will grab the first file in the directory
+	// "*" can be changed if you only want to look for specific files
+	intptr_t handle{ _findfirst("*", &fileinfo) };
+
+	if (handle == -1)  // No files or directories found
+	{
+		perror("Error searching for file");
+		return 0;
+	}
+
+	do
+	{
+		if (strcmp(fileinfo.name, ".") == 0 || strcmp(fileinfo.name, "..") == 0)
+			continue;
+
+		if (fileinfo.attrib & _A_SUBDIR) {
+			std::array<char, 5120> sub_dir{};
+
+			sprintf(sub_dir.data(), "%s%s\\", dir.c_str(), fileinfo.name);
+			dir_list.push_back(std::string{ sub_dir.data() });			
+		}
+		else {
+			std::array<char, 5120> sub_dir{};		
+
+			sprintf(sub_dir.data(), "%s%s", dir.c_str(), fileinfo.name);
+			file_list.push_back(std::string{ sub_dir.data() });			
+		}
+	} while (_findnext(handle, &fileinfo) == 0);
+
+	_findclose(handle); // Close the stream
+	_chdir(original_directory.data());
 	return 0;
 }
 
@@ -78,3 +116,5 @@ int WindowsPlatform::delete_file(const std::string &file_name) {
 	std::unique_ptr<wchar_t[]> file_name_ptr{ get_wide_char_string(file_name.c_str()) };		
 	return static_cast<int>(::DeleteFile(file_name_ptr.get()));
 }
+
+
